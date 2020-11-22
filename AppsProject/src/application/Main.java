2e824fc4bb48d1533af09f9d7520b407d5fe8e14
sethgibb <71202052+sethgibb@users.noreply.github.com>
2020-11-22 01:11:39 -0600

    Update Main.java
    trying to fix conflicts

--- AppsProject/src/application/Main.java
+++ AppsProject/src/application/Main.java
@@ -3,7 +3,8 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.fxml.FXMLLoader;
 
 
@@ -11,9 +12,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("/application/view/Main.fxml"));
+			Scene scene = new Scene(root,430,350);
+			scene.getStylesheets().add(getClass().getResource("/application/view/application.css").toExternalForm());
+			primaryStage.setResizable(false);
+			primaryStage.setTitle("Sports Star Scheduling");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
